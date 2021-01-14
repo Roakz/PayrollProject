963980cb049095f22c12c3a854cbe1d957857cba
rory_ <rory_@LAPTOP-8LGUUB0K>
2021-01-14 12:18:25 +1000

    The staff inheriting admin class is now complete.

--- PayrollProject/Program.cs
+++ PayrollProject/Program.cs
@@ -96,4 +96,32 @@ namespace PayrollProject
             return base.ToString() + $", Allowance: {Allowance}";
         }
     }
+
+    //The admin class also inherits from the Staff class and has  the additional fields of overtimeRate and adminHourlyRate.
+    //The Calculate pay method overrides the base again using these fields to calculate an admins pay.
+    class Admin : Staff
+    {
+        private const float overtimeRate = 15.5f;
+        private const float adminHourlyRate = 30;
+
+        public float Overtime { get; private set; }
+
+        public Admin(string name) : base(name, adminHourlyRate) { }
+
+        public override void CalculatePay()
+        {
+            base.CalculatePay();
+            if(HoursWorked > 160)
+            {
+                Overtime = overtimeRate * (HoursWorked - 160);
+                TotalPay += Overtime;
+            }
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $", Overtime: {Overtime}";
+        }
+
+    }
 }
